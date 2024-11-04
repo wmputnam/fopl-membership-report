4c157b31fcf92e617596dfbec0eedc5e8554f18e
William Putnam <wmputnam@gmail.com>
2024-11-04 11:02:37 -0800

    files from BIRT designer

---
+++ resources/custom.js
@@ -0,0 +1,87 @@
+const toNameTitleCase = ( name) => { 
+    const nameParts = name.split(" ");
+
+    // console.log(nameParts);
+    var newName = new String("");
+    var pad = "";
+
+    for( var s of nameParts) {
+        if( s.includes("-")) {
+            let sarr = s.split("-");
+            let sarrMapped = sarr.map( (item) => item.slice(0,1).toUpperCase() + item.slice(1).toLowerCase() );
+            let sModified = sarrMapped.join("-");
+            newName = `${newName}${pad}${sModified}`;
+        } else if(s.toLowerCase() === "and"){
+            newName = `${newName}${pad}&`;
+        } else if (["OD","MD","JR"].includes(s)) {
+            newName = `${newName}, ${s}`;
+        }else {
+            newName = `${newName}${pad}${s.slice(0,1).toUpperCase()}${s.slice(1).toLowerCase()}`;
+        }
+        pad = " ";
+    }
+    return `${newName}`;
+}
+
+// console.log(toNameTitleCase("bill putnam"));
+
+// ***
+
+const fullNameData = (dataSetRow) => {
+    if( dataSetRow["lastName"] !== null ) {
+        var fullName = dataSetRow["firstName"] === null ? dataSetRow["lastName"] : dataSetRow["firstName"] + " " + dataSetRow["lastName"];
+        return toNameTitleCase(fullName);
+    } else {
+        return "";
+    }
+};
+
+const sepPat = new RegExp(/[,;]/g);
+const emailPhoneData = ( dataSetRow) => {
+    var emailVal = dataSetRow["email"] === null ? "" : dataSetRow["email"].toLowerCase().replaceAll("; ","\n");
+    var phoneVal = dataSetRow["phone"] === null ? "" : dataSetRow["phone"].toLowerCase().replaceAll(sepPat," ").replaceAll(/\s+/g,"\n");
+    return [emailVal,phoneVal].join("\n");
+};
+
+const paidThroughData = ( dataSetRow ) => {	
+    // console.log(dataSetRow)
+    var mmbVal = dataSetRow["mmb"] === null ? "vol" : dataSetRow["mmb"].toLowerCase();
+
+	// var j = dataSetRow["joined"] === null ? "" : dataSetRow["joined"];
+	
+    var p = dataSetRow["paidThrough"] === null ? "" : dataSetRow["paidThrough"].toString().slice(2);
+	switch(mmbVal) {
+		case "":
+   		case "vol":
+   			return "VOL";
+   			
+		case "lm":
+        case "ben":
+		    var result = "LM";
+    		return result;
+		
+        case "hlm":
+                var result = "HLM";
+                return result;
+
+        default:
+	    	var result = p;
+    		return result;
+	};
+};
+
+const addressData = ( dataSetRow ) => {
+
+    if( dataSetRow["address"] !== null ) {
+        var addrVal = dataSetRow["address"] === null ? "" : dataSetRow["address"];
+        var unitVal = dataSetRow["unit"] === null ? "" : " "+dataSetRow["unit"];
+        var stateVal = dataSetRow["state"] === null ? "" : dataSetRow["state"];
+        var postalCodeVal = dataSetRow["postalCode"] === null ? "" : " "+dataSetRow["postalCode"].slice(0,5);
+        return addrVal + unitVal + "\n" + stateVal + postalCodeVal;
+    } else {
+        return "";
+    }
+};
+
+// #A0FBF9
+// #D5D5D5
